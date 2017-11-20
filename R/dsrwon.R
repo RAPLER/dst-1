408838563e0e1453c6a68dea06691ae2b9297ae8
Claude Boivin <webapp.cb@gmail.com>
2017-11-20 15:59:31 -0500

    Update of "dsrwon" function

--- R/dsrwon.R
+++ R/dsrwon.R
@@ -1,39 +1,75 @@
-dsrwon<-function(x,y) {
-  # x1 et y1 doivent être des matrices pour assurer le type numérique dans inters()
-  x1<-as.matrix(x$DempsterRule) 
-  y1<-as.matrix(y$DempsterRule)
-  nc<-ncol(x1)-1
-  N12<-inters(x1,y1)
-  V12<-combmasses(x1,y1)
-  ## transformer tableau : MxN lignes by K hypothesis
+#' Combination of two belief functions
+#' 
+#' The unnormalized Dempster's rule is used to combine two belief functions Bel1 and Bel2 defined on the same frame of discernment and represented by their respective basic chance assignments \code{x} and \code{y}. Dempster's rule of combination is applied. The normalization is not done, leaving the choice to the user to normalize the results (See \code{\link{nzdsr}}) or not.
+#' @details A function (inters) does the intersection of the propositions. Another function (doubles) remove duplicate elements from the result of the intersection of the M propositions of the first bca with the N propositions of the second bca. The two bcas \code{a} and \code{b} must be defined on the same frame of discernment for the intersection of propositions to be meaningful.   
+#' @param x A belief function in its bca form (see \code{\link{bca}}).
+#' @param y A belief function represented by its bca form.
+#' @param infovar A two column matrix containing the variable numbers and the size of the product frame of discernment of each variable.
+#' @param relnb Set at NULL.
+#' @return A list of five elements: \itemize{
+#'   \item $combination: The matrix f of focal elements with a column of masses added.
+#'   \item $I12 A table of intersections between subsets.
+#'   \item $sort_order Indices for the sort of the propositions.
+#'   \item $con: the measure of conflict between beliefs.
+#'   \item $n: the node number.
+#'  \item $inforel. A two column matrix containing variable numbers and the depth of the relation.
+#'   }
+#' @author Claude Boivin, Stat.ASSQ
+#' @export
+#' @examples 
+#' x <- bca(f=matrix(c(0,1,1,1,1,0,1,1,1),nrow=3, byrow = TRUE), m=c(0.2,0.5, 0.3), cnames =c("a", "b", "c"), n=1)
+#' y <- bca(f=matrix(c(1,0,0,1,1,1),nrow=2, byrow = TRUE), m=c(0.6, 0.4),  cnames = c("a", "b", "c"), n=1)
+#' dsrwon(x,y)
+#' @references Shafer, G., (1976). A Mathematical Theory of Evidence. Princeton University Press, Princeton, New Jersey, p. 57-61: Dempster's rule of combination.
+dsrwon<-function(x,y, infovar = NULL, relnb = NULL) {
+  if ( (inherits(x, "bcaspec") == FALSE) | (inherits(y, "bcaspec") == FALSE)) {
+    stop("Error in input arguments: one or more inputs not of class bcaspec.")
+  }
+  x1<-rbind(x$combination[,-1])  # (M x K) matrix
+  y1<-rbind(y$combination[,-1])  # (N x K) matrix
+  nc1<-ncol(x1)
+  nc2<-ncol(y1)
+  if (nc1 != nc2) {
+    stop("Error in input arguments: bca's not on the same frame of discernment.") 
+  }
+  varnb <- x$infovar[,1]
+  N12<-inters(x1,y1)         # intersection of the subsets
+  V12<-outer(x$combination[,1],y$combination[,1], "*")     # compute masses
+  ## transform table: (M x N) rows by K 
   N12<-aperm(N12,c(2,1,3))
-  N12<-array(c(N12),c(dim(N12)[1],prod(dim(N12)[-1]))) ##tab k x (MxN)
+  N12<-array(c(N12),c(dim(N12)[1],prod(dim(N12)[-1])), dimnames= list(colnames(x1), as.vector(outer(rownames(x1), rownames(y1) , FUN="paste"))))
+  
   N12<-aperm(N12,c(2,1)) 
-  ## remove duplicates from the table
-  W1<- doubles(N12)
-  ## idendify  contributions to each subset
-  I12<-dotprod(W1,aperm(N12,c(2,1)),g="&",f="==")
-  ## calcul de la masse totale de chaque ss-ensemble 
-  MAC<-apply(I12*t(array(V12,dim(t(I12)))),1,sum)
-  ## order the subsets to check if and where the empty subset is there
-  TRI<-order(apply(W1,1,sum))
-  z<- sum(W1[TRI[1],])
-  ## identify the indice of the empty set 
-  if (z==0) IVIDE<-TRI[1] else IVIDE<-0 
- ## if (z==0) W2<-rbind(rep(0,times=nc),matrix(W1[TRI[-1],],ncol=nc))  else W2<-matrix(W1,ncol=nc)
-  W2<-matrix(W1,ncol=nc)
-  ## calcul de MVIDE
-  if (z==0) MVIDE<-MAC[IVIDE] else MVIDE<-0
+  W1<- doubles(N12)      ## remove duplicates from the table
+  rownames(W1) <- nameRows(W1)
+  I12<-dotprod(W1,aperm(N12,c(2,1)),g="&",f="==")    ## idendify  contributions to each subset
+  MAC<-apply(I12*t(array(V12,dim(t(I12)))),1,sum)     ## calculate total mass of each subset 
+  ## order the subsets to check if there is the empty subset and where it is.
+  sort_order<-order(apply(W1,1,sum))
+  z<- sum(W1[sort_order[1],])
+  ## identify the place of the empty set 
+  if (z==0) empty<-sort_order[1] else empty<-0 
+  tt<-matrix(W1,ncol=nc1, dimnames = dimnames(W1))
+  ## calculate the mass of the empty set
+  if (z==0) m_empty<-MAC[empty] else m_empty<-0
+  ## Measure of conflict. Code to be revised
   con12<-1-(1-x$con)*(1-y$con)
-  con<-1-(1-con12)*(1-MVIDE)
-   ## result
-  W2<-cbind(matrix(MAC,ncol=1),W2)
-  colnames(W2)<-colnames(x1)
- # result: list m12 of four elements:
- # 1: un-normalized Dempster's rule of combination
- # 2: table of intersections
- # 3: indices for the sort
- # 4: measure of conflict between beliefs
-  list(combination=W2,I12=I12,TRI=TRI,con=con)
+  con<-1-(1-con12)*(1-m_empty)
+  ## result
+  mMAC <-matrix(MAC,ncol=1, dimnames =list(NULL, "mass"))
+  spec <- cbind((1:nrow(tt)), mMAC)
+  colnames(spec) <- c("specnb", "mass")
+  if (missing(infovar)) {
+    infovar <- matrix(c(x$n, ncol(tt)), ncol = 2)
+    colnames(infovar) <- c("varnb", "size")
+  }
+  infovaluenames <- x$infovaluenames
+  if (missing(relnb)) { relnb <- 0 }
+    inforel <- matrix(c(relnb, nrow(infovar)), ncol = 2)
+    colnames(inforel) <- c("relnb", "depth")
+   W2<-cbind(mMAC,tt)
+  z <- list(combination=W2,con = con, n = x$n, tt=tt, spec = spec, infovar = infovar, infovaluenames = infovaluenames, inforel = inforel, I12=I12, sort_order=sort_order)
+  class(z) <- append(class(z), "bcaspec")
+  return(z)
   }
  
