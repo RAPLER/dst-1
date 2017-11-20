388e8cc132a55df68d21bfb81b172a9be70fe2d2
Claude Boivin <webapp.cb@gmail.com>
2017-11-20 16:29:59 -0500

    Update of "initsing" fn with Roxygen

--- R/initsing.R
+++ R/initsing.R
@@ -1,11 +1,19 @@
-initsing<-function(card, names) { 
-  # initialize results
-  # card is the number of elements of the frame
-  # names is a character vector of names
-  x1<-diag(1,card)
-  x2<-matrix(rep(1,times=card),nrow=1)
+#' Initialization of a frame of discernment
+#' 
+#' The purpose of this function is to initialize the basic chance assignment of all the singletons of a frame at a starting value of 0. Each singleton has a zero mass. The matrix of singletons is completed by the frame which receive a mass of one. One use of this function is to apply the plausibility transformation to a belief function and obtain its corresponding probability distribution (See \code{\link{plautrans}}). 
+#' @param size The number of elements of the frame of discernment.
+#' @param names a character vector of the names of the elements of the frame.
+#' @return  A list in bca form, namely a matrix of n+1 rows for the n singletons and the whole set. The measure of conflict and node number are set at 0.
+#' @author Claude Boivin, Stat.ASSQ
+#' @examples
+#' initsing(3,c("a","b","c"))
+#' @export
+#' 
+initsing<-function(size, names) { 
+  x1<-diag(1,size)
+  x2<-matrix(rep(1,times=size),nrow=1)
   f<-rbind(x1,x2)
-  v<-matrix(c(rep(0,times=card),1),ncol=1)
-  y<-bca(v,f,names)
+  v<-matrix(c(rep(0,times=size),1),ncol=1)
+  y<-bca(f,v,cnames=names)
   return(y)      
 }
