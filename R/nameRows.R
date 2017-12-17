ab935c4e260233883444df4a9401a60dd87705a2
Claude Boivin <webapp.cb@gmail.com>
2017-12-16 20:13:26 -0500

    Tests for function "nameRows"

--- R/nameRows.R
+++ R/nameRows.R
@@ -2,26 +2,42 @@
 #' 
 #'  This function uses the column names  of a matrix \code{f} of focal elements to construct the names of the rows.
 #'  
-#' @param f A matrix constructed in a boolean style (0,1) or a boolean matrix. The number of columns of the matrix must match the number of elements of the frame of discernment \eqn{\Theta}. See \code{\link{bca}} for details.
-#' @return The result is a character vector of the names of the focal elements of the matrix \code{f}. The length of the result is \code{nrows(f)}. The last element of the vector is called "frame", to avoid to long a string. The empty set is named "u00f8". The "+" sign is used instead of the "|" to represent the logical "or" operation.
+#' @param f A boolean matrix or a matrix constructed in a boolean style (0,1). The names of the columns are the elements of a frame of discernment \eqn{\Theta}.
+#' @return The result is a character vector of the names of the focal elements of the matrix \code{f}. The length of the result is \code{nrows(f)}. The set of all elements is called "frame", to avoid to long a string. The empty set is named "u00f8". The "+" sign is used instead of the "|" to represent the logical "or" operation.
 #' @author Claude Boivin, Stat.ASSQ
 #' @examples 
-#' f1 <- matrix(c(0,0,0,1,0,0,0,0,1,1,0,1,1,1,1),ncol=3, byrow = TRUE)
-#' colnames(f1) <- c("A","B","C")
-#' rownames(f1) <-nameRows(f1)
-#' f1
+#' f <- matrix(c(0,0,0,1,0,0,0,0,1,1,0,1,1,1,1),ncol=3, byrow = TRUE)
+#' colnames(f) <- c("A","B","C")
+#' rownames(f) <-nameRows(f)
+#' f
+#' f2 <- matrix(c(0,0,0,1,0,0,0,0,1,1,0,1),ncol=3, byrow = TRUE)
+#' colnames(f2) <- c("A2","B2","C2")
+#' rownames(f2) <-nameRows(f2) 
+#' f2
 #' @export
 #' 
 nameRows<-function(f) {
+  if (is.matrix(f) == FALSE) {
+    stop("Input is not a matrix.")
+  }
+  if ( sum((f >1) > 0) > 0) {
+    stop("Input is not a logical or (0,1) matrix.")
+  }
+  f <- f > 0
   # find non zero positions (identification of hypothesis)
-  pos<-f*col(f) 
   names<-colnames(f)
+  if (is.null(names) == TRUE) {
+    stop("No column names supplied.")
+  }
+  pos<-f*col(f) 
+  n_colnames <- apply(pos >0, 1,"sum")
   fun2 <- function(pos, names) {paste(names[pos],collapse=" + ")}
   idFocal <- apply(pos, 1, FUN = fun2, names=names)
-  idFocal[length(idFocal)] <- "frame"
   if (sum((idFocal=="")*1:length(idFocal)) > 0) {
- # idFocal[(idFocal=="")*1:length(idFocal)] <- "ø"
-    idFocal[(idFocal=="")*1:length(idFocal)] <- "\u00f8"
+    idFocal[(idFocal=="")*1:length(idFocal)] <- "\u00f8" # "ø" code
+  }
+  if (sum(nnames_idFocal == ncol(pos)) > 0) {
+    idFocal[(n_colnames== ncol(pos))*(1:length(idFocal))] <- "frame"
   }
   return(idFocal)
 }
