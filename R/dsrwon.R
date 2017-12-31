7799d2e81c403f98936dafa9d179393c3d3cfe42
Claude Boivin <webapp.cb@gmail.com>
2017-12-30 21:50:45 -0500

    Test of dsrwon function.

--- R/dsrwon.R
+++ R/dsrwon.R
@@ -1,11 +1,10 @@
 #' Combination of two belief functions
 #' 
-#' The unnormalized Dempster's rule is used to combine two belief functions Bel1 and Bel2 defined on the same frame of discernment and represented by their respective basic chance assignments \code{x} and \code{y}. Dempster's rule of combination is applied. The normalization is not done, leaving the choice to the user to normalize the results (See \code{\link{nzdsr}}) or not.
+#' The unnormalized Dempster's rule is used to combine two belief functions Bel1 and Bel2 defined on the same frame of discernment and represented by their respective basic chance assignments \code{x} and \code{y}. Dempster's rule of combination is applied. The normalization is not done, leaving the choice to the user to normalize the results or not (See \code{\link{nzdsr}}).
 #' @details A function (inters) does the intersection of the propositions. Another function (doubles) remove duplicate elements from the result of the intersection of the M propositions of the first bca with the N propositions of the second bca. The two bcas \code{a} and \code{b} must be defined on the same frame of discernment for the intersection of propositions to be meaningful.   
 #' @param x A belief function in its bca form (see \code{\link{bca}}).
 #' @param y A belief function represented by its bca form.
-#' @param infovar A two column matrix containing the variable numbers and the size of the product frame of discernment of each variable.
-#' @param relnb Set at NULL.
+#' @param infovarname A name can be given to the resulting variable. Named "v1" if missing.
 #' @return A list of five elements: \itemize{
 #'   \item $combination: The matrix f of focal elements with a column of masses added.
 #'   \item $I12 A table of intersections between subsets.
@@ -14,23 +13,24 @@
 #'   \item $n: the node number.
 #'  \item $inforel. A two column matrix containing variable numbers and the depth of the relation.
 #'   }
+#'   @details The relation number of the x input is given to the result.
 #' @author Claude Boivin, Stat.ASSQ
 #' @export
 #' @examples 
-#' x <- bca(f=matrix(c(0,1,1,1,1,0,1,1,1),nrow=3, byrow = TRUE), m=c(0.2,0.5, 0.3), cnames =c("a", "b", "c"), n=1)
-#' y <- bca(f=matrix(c(1,0,0,1,1,1),nrow=2, byrow = TRUE), m=c(0.6, 0.4),  cnames = c("a", "b", "c"), n=1)
-#' dsrwon(x,y)
+#' x <- bca(f=matrix(c(0,1,1,1,1,0,1,1,1),nrow=3, byrow = TRUE), m=c(0.2,0.5, 0.3), cnames =c("a", "b", "c"), infovarnames = "x", n=1)
+#' y <- bca(f=matrix(c(1,0,0,1,1,1),nrow=2, byrow = TRUE), m=c(0.6, 0.4),  cnames = c("a", "b", "c"), infovarnames = "y", n=1)
+#' dsrwon(x,y, infovarnames = "xy")
 #' @references Shafer, G., (1976). A Mathematical Theory of Evidence. Princeton University Press, Princeton, New Jersey, p. 57-61: Dempster's rule of combination.
-dsrwon<-function(x,y, infovar = NULL, relnb = NULL) {
+dsrwon<-function(x,y, infovarnames = NULL) {
   if ( (inherits(x, "bcaspec") == FALSE) | (inherits(y, "bcaspec") == FALSE)) {
-    stop("Error in input arguments: one or more inputs not of class bcaspec.")
+    stop("One or more inputs not of class bcaspec.")
   }
-  x1<-rbind(x$combination[,-1])  # (M x K) matrix
-  y1<-rbind(y$combination[,-1])  # (N x K) matrix
+  x1<-rbind(x$tt)  # (M x K) matrix
+  y1<-rbind(y$tt)  # (N x K) matrix
   nc1<-ncol(x1)
   nc2<-ncol(y1)
   if (nc1 != nc2) {
-    stop("Error in input arguments: bca's not on the same frame of discernment.") 
+    stop("Nb of elements of frame x and frame y not equal.") 
   }
   varnb <- x$infovar[,1]
   N12<-inters(x1,y1)         # intersection of the subsets
@@ -59,15 +59,22 @@ dsrwon<-function(x,y, infovar = NULL, relnb = NULL) {
   mMAC <-matrix(MAC,ncol=1, dimnames =list(NULL, "mass"))
   spec <- cbind((1:nrow(tt)), mMAC)
   colnames(spec) <- c("specnb", "mass")
-  if (missing(infovar)) {
-    infovar <- matrix(c(x$n, ncol(tt)), ncol = 2)
-    colnames(infovar) <- c("varnb", "size")
+  # infovar parameter
+  infovar <- matrix(c(x$n, ncol(tt)), ncol = 2)
+  colnames(infovar) <- c("varnb", "size")
+  # infovaluenames parameter
+  cnames <- colnames(W1)
+  if (missing(infovarnames)) {
+    infovaluenames <- split(cnames, rep(paste(rep("v", nrow(infovar)),c(1:nrow(infovar)),sep=""), infovar[,2]))
+  } else {
+    infovaluenames <- split(cnames,rep(infovarnames, infovar[,2]))
   }
-  infovaluenames <- x$infovaluenames
-  if (missing(relnb)) { relnb <- 0 }
-    inforel <- matrix(c(relnb, nrow(infovar)), ncol = 2)
-    colnames(inforel) <- c("relnb", "depth")
-   W2<-cbind(mMAC,tt)
+  # inforel parameter
+  relnb <- (x$inforel)[,1]
+  inforel <- matrix(c(relnb, nrow(infovar)), ncol = 2)
+  colnames(inforel) <- c("relnb", "depth")
+  # construction of the result
+  W2<-cbind(mMAC,tt)
   z <- list(combination=W2,con = con, n = x$n, tt=tt, spec = spec, infovar = infovar, infovaluenames = infovaluenames, inforel = inforel, I12=I12, sort_order=sort_order)
   class(z) <- append(class(z), "bcaspec")
   return(z)
