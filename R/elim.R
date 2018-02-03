66157d8d81a1fed96a31b41462d48726a9d1b1dd
Claude Boivin <webapp.cb@gmail.com>
2018-02-02 20:35:10 -0500

    Function elim This goal of this function is to reduce a relation defined on a product space of n variables to its corresponding relation on the space of the n-1 remaining variables. We choose the variable to eliminate and the relation on the reduced product space is computed.

--- R/elim.R
+++ R/elim.R
@@ -1,37 +1,74 @@
 #'  Reduction of a relation
 #' 
-#' This function eliminates a chosen variable from a relation and calculate the bcaspec of the remaining variables.
-#' @param rel the relation to modify
-#' @param x the number of the variable to eliminate
-#' @return The reducted relation
+#' This function starts with a relation defined on a product space. Having chosen a variable to eliminate, the reduced product space is determined and the corresponding bcaspec is claculated.
+#' @param rel the relation to marginalize, an object of class bcaspec.
+#' @param xnb Identification of the variable to eliminate by its number.
+#' @return The reduced relation
 #' @author Claude Boivin, Stat.ASSQ
 #' @export
-#' 
-elim <- function(rel, x) {
-  varnb <- rel$infovar[,1]         # numbers of the variables 
-  varRank <- match(x, varnb)       # rank of the variable
-  dim_to_keep <- (1:length(varnb))*!varnb %in% x # axes to keep
-  n <- nrow(rel$tt)
-  m <- rel$spec[,2]                # extract mass vector
+#' @examples  
+#' rel_tt <- matrix(c(0,1,rep(0,5),rep(c(1,0),2),1,1,0,1,0,rep(1,3),0,1,0,rep(1,6)), ncol=4, byrow = TRUE)
+#' colnames(rel_tt) <- c("rWdy Ry", "rWdy Rn", "rWdn Ry", "rWdn Rn")
+#'  rel_spec = matrix(c(1:7, 0.0476, 0.7619, 0.1905, 0,0,0,0), ncol = 2, dimnames = list(NULL, c("specnb", "mass"))) 
+#'  rel_infovar = matrix(c(4,5,2,2), ncol = 2, dimnames = list(NULL, c("varnb", "size")) )
+#'  rel_rel <- list(tt=rel_tt, con=0.16, spec=rel_spec, infovar=rel_infovar, infovaluenames= list(Rain=c("Ry", "Rn"), RdWorks=c("rWdy", "rWdn") ))
+#'  class(rel_rel)="bcaspec"
+#'  elim(rel_rel, xnb = 5)
+#'  elim(rel_rel, xnb = 4)
+#'  
+#'  mrt_tt <- matrix(c(1,0,1,0,0,1,1,0,0,1,0,1,1,0,0,1,0,1,1,0,0,1,0,1,rep(1,4)), ncol=4, byrow = TRUE)
+#' colnames(mrt_tt) <- c("t6", "f6", "t8", "f8")
+#'  mrt_spec = matrix(c(1,1,1,2,2,2,3, 0.1, 0.1, 0.1, 0.7,0.7,0.7,0.2), ncol = 2, dimnames = list(NULL, c("specnb", "mass"))) 
+#'  mrt_infovar =matrix(c(6,8,2,2), ncol = 2, dimnames = list(NULL, c("varnb", "size")) )
+#'  mrt_rel <- bcaRel(tt=mrt_tt, spec=mrt_spec, infovar=mrt_infovar, infovarnames= c("Maintenance", "Repair") )
+#'  elim(mrt_rel, xnb = 6)
+#'  elim(mrt_rel, xnb = 8)
+elim <- function(rel, xnb) {
+  if (inherits(rel, "bcaspec") == FALSE)  {
+    stop("Rel input not of class bcaspec.")
+  }
   size_vars <- rel$infovar[,2]    # vector of the sizes of axes
-  itab <-array(t(rel$tt), c(size_vars, n)) # restructure data
-  itab <- aperm(itab, c(2,1,(1+length(varnb))))
-  fun3 <- function(x, oper) {reduction(x, f=oper)}
-  proj <- t(apply(itab, c(dim_to_keep,(1+length(varnb))), FUN= fun3, oper= "|"))
-z2=proj  # complete code see APL function ELIM
-w1 <- doubles(z2*1) # mult by 1 transforms from logical to numeric
-I12 <- dotprod(w1, t(z2), g = "&", f = "==")
-m1<- t(array(m,c(ncol(I12), nrow(I12))))
-m1 <- apply(I12*m1, 1, sum)
-tt=w1
-idnames <- names(rel$infovaluenames)[varRank]
-colnames(tt) <- rel$infovaluenames[[idnames]]
-# varnb <- rel$infovar[,1 ]
-infovar <- subset(rel$infovar, (as.logical(dim_to_keep)))
-infovaluenames <- subset(rel$infovaluenames, (as.logical(dim_to_keep)))
-colnames(infovar) <- c("varnb", "size")
-inforel <- matrix(c(rel$inforel[,1], rel$inforel[,2]-1), ncol = 2)
-colnames(inforel) <- c("relnb", "depth")
- r <- bca(f = tt, m = m1, cnames = rownames(w1), con = rel$con, infovar = infovar, infovaluenames = infovaluenames, inforel = inforel)
+  nbvar <- length(size_vars)
+  size_vars_inv <- size_vars[nbvar:1]          # same order as APL prog
+  varnb <- rel$infovar[,1]         # numbers of the variables 
+  varnb_inv <- varnb[nbvar:1]          # same order as APL prog
+  varRank <- match(xnb, varnb_inv)       # rank of the variable
+  if(is.na(varRank)) {
+    stop("Invalid variable number.")
+  }
+   dim_to_keep <- (1:length(varnb_inv))*!varnb_inv %in% (xnb) # axes to keep
+  n <- nrow(rel$tt)
+  m <- as.vector(unlist(rel$spec[,2]) )   # extract mass vector
+  itab <-array(t(rel$tt), c(size_vars_inv,n)) # restructure data
+  fun3 <- function(xnb, oper) {reduction(xnb, f=oper)}
+  proj <- apply(itab, c(dim_to_keep,(1+nbvar)), FUN= fun3, oper= "|")
+  # transformer proj en matrice
+  z2<- t(matrix(as.vector(proj), ncol=n, nrow =prod(dim(proj)[-length(dim(proj))])))
+  w1 <- doubles(z2*1) # mult by 1 transforms from logical to numeric
+  I12 <- dotprod(w1, t(z2), g = "&", f = "==")
+  m1<- t(array(m,c(ncol(I12), nrow(I12))))
+  m1 <- apply(I12*m1, 1, sum)
+  tt=w1
+# naming the columns of tt matrix
+  var_to_keep <- varnb*!varnb %in% (xnb)
+  var_to_keep <- var_to_keep[var_to_keep>0]
+  varRank_to_keep <- match(var_to_keep, varnb)
+  idnames <- names(rel$infovaluenames)[varRank_to_keep]
+  znamesCols <- matrix(rel$infovaluenames[[varRank_to_keep[1]]], ncol = 1)
+if (length(idnames) > 1) {
+  for (i in 2:length(idnames)) {
+    ci <-matrix(rel$infovaluenames[[varRank_to_keep[i]]], ncol = 1)
+    znamesCols <- dotprod(znamesCols, t(ci), "paste", "paste")  # pour dotprod des noms
+    znamesCols <-t(matrix(t(znamesCols), ncol = prod(dim(znamesCols))))
+  } }
+  colnames(tt) <- as.vector(znamesCols)
+# End naming cols
+# infovar parameter
+ infovar <- matrix(rel$infovar[varRank_to_keep,], ncol=2)
+# infovaluenames parameter
+  infovaluenames <- rel$infovaluenames[varRank_to_keep]
+# inforel parameter
+  inforel <- rel$inforel
+  r <- bca(f = tt, m = m1, cnames = rownames(w1), con = rel$con, infovar = infovar, infovarnames = idnames, infovaluenames = infovaluenames, inforel = inforel)
 return(r)
 }
---
+++ tests/testthat/test_elim.R
@@ -0,0 +1,13 @@
+# Tests "elim" function
+test_that("elim", {
+  # T1 rel must be of class bcaspec. 
+  x1 <- list(f=matrix(c(1,0,0,1,1,1),nrow=2, byrow = TRUE), m=c(0.6, 0.4),  cnames = c("a", "b", "c"),  infovarnames = "y1", n=1)
+  expect_error(elim(x1, 4) , "Rel input not of class bcaspec.")
+  #
+  # T2 The number xnb must be in the list of numbers of the relation (variable varnb of the infovar table). 
+  ttrwf= matrix(c(0,1,1,0,1,0,1,0,1,0,0,1,1,1,1,1),nrow=4, byrow = TRUE, dimnames =list(NULL, c("rWdy", "rWdn", "Ry", "Rn")) )
+  specrw = matrix(c(1,1,1,2,0.75,0.75,0.75,0.25), ncol = 2, dimnames = list(NULL, c("specnb", "mass"))) 
+  inforw =matrix(c(4,5,2,2), ncol = 2, dimnames = list(NULL, c("varnb", "size")) )
+  x2 <- bcaRel(tt = ttrwf, spec = specrw, infovar = inforw, infovarnames = c("RdWorks", "Rain"), relnb = 6)
+  expect_error(elim(x2, 1), "Invalid variable number.")
+})
