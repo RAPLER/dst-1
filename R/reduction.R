c082c9c5de0b025411faecbfe5cd76c6af17e825
Claude Boivin <webapp.cb@gmail.com>
2018-03-03 21:42:19 -0500

    Utility function reduction Documentation and tests

--- R/reduction.R
+++ R/reduction.R
@@ -1,18 +1,25 @@
-#' Obtain the summary of a vector for any operator.
+#' Summary of a vector for any operator.
 #' 
-#' @export
+#' This utility function is used to obtain a summary of a vector of data for many operators. This code has been adapted from the \code{aplRDV} R function of Jan de Leeuw.
 #' @aliases aplRDV
-#' \code{reduction} returns...
-#' @param x The data...
-#' @param f The operator.
-#' @author Original author of the aptRDV function: Jan de Leeuw 
+#' @param x A vector of numbers or strings.
+#' @param f The operator. Must be compatible with the type of vector (numeric or character)
+#' @return The result of applying the chosen operator to all the elements of the vector is an object of length 1.
+#' @author \itemize{
+#'  \item Claude Boivin, Stat.ASSQ. 
+#'  \item Original author of the aptRDV function: Jan de Leeuw 
 #'       \url{http://www.codecollector.net/view/8A8D9395-0F66-4706-A23E-C588151E8423-95744-0000429BCF33A153}.
+#'       }
 #' @seealso Jan de Leeuw and Masanao Yajima: \url{https://rpubs.com/deleeuw/158476}.
 #' @references \itemize{
 #'   \item G. Helzer. (1989): \emph{An Encyclopedia of APL}, second edition, I-APL LTD, St. Albans, G.B.
 #'  \item  L. Gilman and A. J. Rose.(1974): \emph{APL an Interactive Approach}, Second Edition, John Wiley, New-York.
 #'  }
+#'  @export
 #'  @examples
+#'  reduction(c(1,2,3,4), f="-")
+#'  reduction(c(1,0,1,1,0), f="|")
+#'  reduction(c("a", "b", "c"), f="paste")
 #'  
 reduction<-function(x,f="+") {
   if (length(x) == 0) return(x)
