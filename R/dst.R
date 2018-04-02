d303afe37a430f546dc8dd8b8057b0a7d2ec3910
Claude Boivin <webapp.cb@gmail.com>
2018-04-01 22:03:10 -0400

    help page for dst

---
+++ R/dst.R
@@ -0,0 +1,18 @@
+#' dst: A package for the computation of belief functions
+#' 
+#' @description The dst package allows you to make basic probability assignments on subsets of a set of possibilities (events) and combine these events with Dempster's rule of combination.
+#' @details The main operations that can be done are: \itemize{
+#' \item definition of a basic chance assignment (bca) distribution on a variable
+#'  \item combination of two bca's defined on the same variable
+#'  \item definition of a bca which establish a relation between two or more variables
+#'  \item extension of a bca
+#'  \item marginalization of a bca
+#'  }
+#'  
+#'  @author Author: Claude Boivin, Stat.ASSQ <webapp.cb@gmail.com>
+#'  
+#'  @maintainer Maintainer: Claude Boivin <webapp.cb@gmail.com>
+#'  
+#' @docType package
+#' @name dst
+NULL
