5d0af78c019f2587b4d77a189199ae18e3084f25
Claude Boivin <webapp.cb@gmail.com>
2018-04-14 10:34:03 -0400

    Correction to documentation of package

--- R/dst.R
+++ R/dst.R
@@ -1,6 +1,6 @@
-#' dst: A package for the computation of belief functions
+#' dst: Manipulation and computation of belief functions
 #' 
-#' @description The dst package allows you to make basic probability assignments on subsets of a set of possibilities (events) and combine these events with Dempster's rule of combination.
+#' @description dst allows you to make basic probability assignments on subsets of a set of possibilities (events) and combine these events with Dempster's rule of combination.
 #' @details The main operations that can be done are: \itemize{
 #' \item definition of a basic chance assignment (bca) distribution on a variable
 #'  \item combination of two bca's defined on the same variable
