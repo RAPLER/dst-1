8c29d02b65593a0ede660da87a8a538adb4ce710
Claude Boivin <webapp.cb@gmail.com>
2023-09-14 16:08:43 -0400

    Development of a new version with sparse matrices and use of subset names to do intetrsections

--- R/dsrwon.R
+++ R/dsrwon.R
@@ -6,6 +6,7 @@
 #' @details The two bca's \code{x} and \code{y} must be defined on the same frame of discernment for the combination to take place. The relation number of the x input is given to the output result.  
 #' @param x A basic chance assignment (see \code{\link{bca}}).
 #' @param y A basic chance assignment (see \code{\link{bca}}).
+#' @param sparseM Put TRUE to indicate the use of sparse matrices as input. Default = FALSE.
 #' @param mcores Make use of multiple cores ("yes") or not ("no"). Default = "no".
 #' @param varnames A character string to name the resulting variable. named "z" if omitted.
 #' @param infovarnames Deprecated. Old name for \code{varnames}.
@@ -26,22 +27,12 @@
 #' cnames = c("a", "b", "c"),  
 #' varnames = "x", idvar = 1)
 #' dsrwon(y1,y2)
-#' x1 <- bca(m = c(0.2,0.5, 0.3), 
-#' varnames = "x", idvar = 1, 
-#' ssnames = list(c("b", "c"), c("a", "b"), c("a", "b", "c")), sfod = 3)
-#' x2 <- bca(m = c(0.6, 0.4),  
-#' varnames = "x", idvar = 1, 
-#' ssnames = list(c("a"), c("a", "b", "c")), sfod = 3)
-#' dsrwon(x1,x2)
 #' vacuous <- bca(matrix(c(1,1,1), nrow = 1), m = 1, cnames = c("a","b","c"))
-#' vacuous <- bca(m = 1,  
-#' varnames = "x", idvar = 1, 
-#' ssnames = list(c("a", "b", "c")), sfod = 3)
-#' ## dsrwon(vacuous, vacuous)
+#' dsrwon(vacuous, vacuous)
 #' @references Shafer, G., (1976). A Mathematical Theory of Evidence. Princeton University Press, Princeton, New Jersey, pp. 57-61: Dempster's rule of combination.
-dsrwon<-function(x, y, mcores = "no", varnames = NULL, relnb = NULL, infovarnames) {
+dsrwon<-function(x, y, sparseM = FALSE, mcores = "no", varnames = NULL, relnb = NULL, infovarnames) {
   #
-  # Local variables: m1, m2, zx, zy, x1, y1, z, zz1, zz2,W1_list, W1s, values1, values2, V12, N12, W1, I12, MAC, nMAC
+  # Local variables: m1, m2, zx, zy, x1, y1, z, zz1 ,W1_list, W1s, values1, values2, V12, N12, W1, I12, MAC, nMAC
   # Functions calls: nameRows, dotprod
   # 0. Catch old parameters names, if any and replace by the new ones
   #
@@ -51,6 +42,7 @@ dsrwon<-function(x, y, mcores = "no", varnames = NULL, relnb = NULL, infovarname
     warning("Parameter name 'infovarnames' is deprecated. Use 'varnames' instead.")
     varnames <- infovarnames
   }
+  #
   # end catches
   #
   ## 1. Checks
@@ -88,7 +80,7 @@ dsrwon<-function(x, y, mcores = "no", varnames = NULL, relnb = NULL, infovarname
   }
   # 2. Computation using tt matrices
   # 2.1. checks specific to the tt matrix 
-  if (!is.null(x$tt)) {
+  if  ((!is.null(x$tt)) & (!is.null(y$tt) ) ) {
   # 
   # 2.1.1. x1 and y1 must have same frame of discernment
   # and same number of elements
@@ -113,39 +105,28 @@ dsrwon<-function(x, y, mcores = "no", varnames = NULL, relnb = NULL, infovarname
   }
   ## 2a.1 Combine mass vectors
   V12<-outer(zx$spec[,2],zy$spec[,2], "*")
-  # # NB refaire ici les tests de L74-L90 avec ssnames
-  # #
-  # ## 2b. Calculations
-  # #
-  # ## 2b.1 Combine mass vectors
-  # #  
-  # V12<-t(outer(zx$spec[,2],zy$spec[,2], "*") )     # compute masses OK, not long.
+  #
+  # # NB refaire ici les tests de L90-L114 avec ssnames
   #
   ## 2.2 combine subsets
   # transform table of intersections: (M x N) rows by K 
   # 
-  # test
+     # Obtain ssnames via tt colnames
   if ((!is.null(zx$tt)) & (!is.null(zy$tt) ) ) {
- # if (is.null(zx$ssnames) | is.null(zy$ssnames) ) {
+     zframe <-colnames(zx$tt)
+     zx$ssnames <- lapply(X=1:nrow(zx$tt), FUN = function(X) {zframe[zx$tt[X,]*1:ncol(zx$tt)]})
+     zy$ssnames <- lapply(X=1:nrow(zy$tt), FUN = function(X) {zframe[zy$tt[X,]*1:ncol(zy$tt)]})
+  }
+  #
+  #  Calculations with tt matrices, non sparse)
+  if (sparseM == FALSE ) {
+  # if ((!is.null(zx$tt)) & (!is.null(zy$tt) ) ) {
+  #
     # 3.0  test 20230727
-    # test avec subset names
-    # Obtain ssnames via tt colnames 
-    #
-    zframe <-colnames(zx$tt)
-    zx$ssnames <- lapply(X=1:nrow(zx$tt), FUN = function(X) {zframe[zx$tt[X,]*1:ncol(zx$tt)]})
-    zy$ssnames <- lapply(X=1:nrow(zy$tt), FUN = function(X) {zframe[zy$tt[X,]*1:ncol(zy$tt)]})
-    #
   # Use multiple cores = "yes"
-#  if  ((mcores == "yes") & (is.matrix(zx$tt) == TRUE) ) {
   if  (mcores == "yes") {
-    if ((isS4(x1) == TRUE ) | (isS4(y1) == TRUE ) ) {
-      y1_df <- Matrix::sparseMatrix(i = (y1@j+1), p = (y1@p), x=TRUE, dims = dim(y1)[2:1], dimnames = list(dimnames(y1)[[2]], dimnames(y1)[[1]]))
-    } 
-    else {
-  y1_df <- as.data.frame(t(y1))
-    }
   ncores <- parallel::detectCores(logical = FALSE)
-  grappe <- parallel::makeCluster(ncores-1)
+  grappe <- parallel::makeCluster(ncores)
  # use devtools for testing
   #
   # test
@@ -153,72 +134,65 @@ dsrwon<-function(x, y, mcores = "no", varnames = NULL, relnb = NULL, infovarname
   # parallel::clusterEvalQ(cl = grappe, expr = library(dst))
   # end test
   #
-  parallel::clusterExport(cl = grappe, varlist = list("x1", "y1_df"), envir = environment() )
-    if (isS4(y1_df) == TRUE) {
-      mx1y1_par <-  parallel::parSapply( cl = grappe, X=1:nrow(y1_df), FUN = function(X) { inters(x1, matrix(y1_df[X,], nrow = 1) ) }, simplify = FALSE, USE.NAMES = TRUE ) # intersection of the subsets
-    } 
+  parallel::clusterExport(cl = grappe, varlist = list("x1", "y1"), envir = environment() )
+  mx1y1_par <-  parallel::parSapply( cl = grappe, X=1:nrow(y1), FUN = function(X) { inters(x1, matrix(y1[X,], nrow = 1) ) }, simplify = FALSE, USE.NAMES = TRUE ) # intersection of the subsets
+  parallel::stopCluster(grappe)
+  # Transformation
+  N12 <- mx1y1_par[[1]]
+    for (i in 2:shape(mx1y1_par) ) {
+      N12 <- rbind(N12, mx1y1_par[[i]] )
+    }
+  }
   else {
-      mx1y1_par <-  parallel::parSapply( cl = grappe, X=1:ncol(y1_df), FUN = function(X) { inters(x1, t(y1_df[X])) }, simplify = FALSE, USE.NAMES = TRUE ) # intersection of the subsets
-    }
-    parallel::stopCluster(grappe)
-  # Transformation 
-      N12 <- mx1y1_par[[1]]
-      for (i in 2:shape(mx1y1_par) ) {
-        N12 <- rbind(N12, mx1y1_par[[i]] )
-      }
-  } 
-    else {
     N12<-inters(x1, y1)         # intersection of the subsets
-    }
+  }
  #
  # Matrix transformation
  #
-    if (isS4(N12) == TRUE ) {
-    N12 <- as.matrix(N12) # convert sparse to matrix. to be able to apply duplicated() operator
-  }
-  #
-  # Remove duplicates from the table
-  W1<- N12[!duplicated(N12),]  ## remove duplicates 
-  if (is.matrix(W1) == FALSE) {
-    W1 <- t(as.matrix(W1))
-  }
-  #
+ if (isS4(N12) == TRUE ) {
+   N12 <- as.matrix(N12) # convert sparse to matrix. to be able to apply duplicated() operator
+ }
+ #
+ # Remove duplicates from the table
+ W1<- N12[!duplicated(N12),]  ## remove duplicates
+ if (is.matrix(W1) == FALSE) {
+   W1 <- t(as.matrix(W1))
+ }
+ #
   ## 2.3 Identify contributions to each subset and compute mass
   #
-#  if ((mcores == "yes") & (is.matrix(zx$tt) == TRUE) ) {
   if  (mcores == "yes") {
-    #test
-  #  z2_df <- as.data.frame(aperm(N12,c(2,1)))
-    # end test
    ncores <-  parallel::detectCores(logical = FALSE)
-   grappe <-  parallel::makeCluster(ncores-1)
+   grappe <-  parallel::makeCluster(ncores)
     # use devtools for testing
     #
     # test
     parallel::clusterEvalQ(cl = grappe, expr = devtools::load_all("."))
     # parallel::clusterEvalQ(cl = grappe, expr = library(dst))
- #   parallel::clusterExport(cl = grappe, varlist = list("W1", "z2_df"), envir = environment() )
     parallel::clusterExport(cl = grappe, varlist = list("W1", "N12"), envir = environment() )
   # end test
   #
-  ## 2.3 Identify contributions to each subset and compute mass
+  I12_par <-  parallel::parSapply( cl = grappe, X=1:nrow(N12), FUN = function(X) {  outer(rownames(W1), rownames(N12)[X], FUN = "==")  }, simplify = FALSE, USE.NAMES = TRUE )
+  parallel::stopCluster(grappe)
   #
-  # test
-   # I12_par <-  parallel::parSapply( cl = grappe, X=1:ncol(z2_df), FUN = function(X) {  dotprod  (W1, as.matrix(z2_df[,X]), g = "&",f = "==")  }, simplify = FALSE, USE.NAMES = TRUE ) 
-    I12_par <-  parallel::parSapply( cl = grappe, X=1:nrow(N12), FUN = function(X) {  outer(rownames(W1), rownames(N12)[X], FUN = "==")  }, simplify = FALSE, USE.NAMES = TRUE ) 
-   parallel::stopCluster(grappe)
-#  }
   # List to array conversion
   I12 <- array(unlist(I12_par), dim = c(shape(I12_par[[1]])[1], shape(I12_par)))
-  } 
+  }
   else {
     I12 <- outer(rownames(W1), rownames(N12), FUN = "==")
     if (isS4(N12) == TRUE) {
-      I12 <- methods::as(I12, "RsparseMatrix")  
+      I12 <- methods::as(I12, "RsparseMatrix")
     }
   }
-   MAC<-apply(I12*t(array(V12,dim(I12)[2:1]) ),1,sum) 
- 
+  #
+ # mmass vector
+  if (mcores == "yes") {
+  MAC<-apply(I12*t(array(V12,dim(I12)[2:1]) ),1,sum)
+  }
+  else {
+    MAC<-apply(I12*t(array(t(V12),dim(I12)[2:1]) ),1,sum)
+  }
+ #
   ## 2.4.1  Order the subsets to find if the empty subset is there. Put empty set in first position of tt matrix
   sort_order <-order(apply(W1,1,sum))
   tt <- W1[sort_order,]
@@ -231,9 +205,9 @@ dsrwon<-function(x, y, mcores = "no", varnames = NULL, relnb = NULL, infovarname
   #
   z<- sum(W1[sort_order[1],])
   if (z==0) {
-    empty<-sort_order[1]  
-    m_empty<-MAC[empty] 
-  } 
+    empty<-sort_order[1]
+    m_empty<-MAC[empty]
+  }
   else {
     empty<-0
     m_empty<-0
@@ -245,80 +219,99 @@ dsrwon<-function(x, y, mcores = "no", varnames = NULL, relnb = NULL, infovarname
   #
   spec <- cbind(1:nrow(tt), mMAC)
   colnames(spec) <- c("specnb", "mass")
-  # 
+  #
   ## 2.7.1  Revised 2023-06-14
   ## measure of contradiction (con). It is simply m_empty, always
-  # 
+  #
   con <- m_empty
-  if  (con == 1) { 
+  if  (con == 1) {
     warning('Totally conflicting evidence (con = 1). Data is inconsistent.')
        }
   } 
   #
   # 3. NEW. Test avec utilisation de subsets names
   #
-  if ((is.null(zx$tt)) | (is.null(zy$tt) ) ) {
- # else {
-    #
-    # Computation with ssnames
+  if (sparseM == TRUE ) {
+  # if ((is.null(zx$tt)) | (is.null(zy$tt) ) ) {
+  #  
     # 3.1.compute N12 and transform
     #
+    # Use multiple cores = "yes"
+    if  (mcores == "yes") {
+      ncores <- parallel::detectCores(logical = FALSE)
+      grappe <- parallel::makeCluster(ncores)
+      # use devtools for testing
+      #
+      # test
+      parallel::clusterEvalQ(cl = grappe, expr = devtools::load_all("."))
+      # parallel::clusterEvalQ(cl = grappe, expr = library(dst))
+      # end test
+      #
+      zzx=zx$ssnames
+      zzy=zy$ssnames
+      parallel::clusterExport(cl = grappe, varlist = list("zzx", "zzy"), envir = environment() )
+      N12 <-  parallel::mcmapply(FUN= function(X,Y) {lapply(X = 1:length(zzx), FUN = function(X) {intersect(zzx[[X]], zzy[[Y]])} )}, Y=1:length(zzy) )  # intersection of the subsets
+      parallel::stopCluster(grappe)
+    }
+    else {
     N12 <-  mapply(FUN= function(X,Y) {lapply(X = 1:length(zx$ssnames), FUN = function(X) {intersect(zx$ssnames[[X]], zy$ssnames[[Y]])} )}, Y=1:length(zy$ssnames) ) 
+    }
     # Transform N12
     # for every element of the list, sequence of elements of the subsets
-    cN12 <- c(N12)
+    cN12 <- c(t(N12) )
     # Obtain ssnames as a list
     cN12c <- lapply(X=1:length(cN12), FUN =  function(X) { reduction(cN12[[X]], f = "paste")})
     # Transform list in character vector
     cN12v <- unlist(lapply(X=1:length(cN12c), FUN = function(X) {if (length(cN12c[[X]]) == 0){ cN12c[[X]] <- "Empty"} else cN12c[[X]] }) )
     #
-    # 3.2 Compute W1 as character vector and list
+    # 3.2 Remove duplicattes in N12 to obtain the result of the intersections (W1 as character vector and list)
     #
     W1 <- cN12v[!duplicated(cN12v)]
     W1_list <- cN12[!duplicated(cN12)]
-     #
-    # Transformations to obtain I12 matrix
-    # Transform W1
     #
-    # W1c <- lapply(X=1:length(W1), FUN =  function(X) { reduction(W1[[X]], f = "paste")})
-    # W1cf <- unlist(lapply(X=1:length(W1c), FUN = function(X) {if (length(W1c[[X]]) == 0){ W1c[[X]] = "Empty"} else W1c[[X]] }) )
-    #
-    # 3.3 Compute I12 matrix
-    #
-    I12 <- outer(W1, cN12v, FUN = "==" )
-   #
-   # 2.4.2  Order the subsets to find if the empty subset is there. Put empty set in first position of ssnames list
-   #
-   # compute masses
-   MAC<-apply(I12*t(array(V12,dim(t(I12)))),1,sum)   
-   #
+    # identify contribution of cN12v to each subset of W1
+    if  (mcores == "yes") {
+      ncores <- parallel::detectCores(logical = FALSE)
+      grappe <- parallel::makeCluster(ncores)
+      # use devtools for testing
+      #
+      # test
+      parallel::clusterEvalQ(cl = grappe, expr = devtools::load_all("."))
+      # parallel::clusterEvalQ(cl = grappe, expr = library(dst))
+      # end test
+      #
+      parallel::clusterExport(cl = grappe, varlist = list("W1", "cN12v"), envir = environment() )
+      I12 <- parallel::parSapply( cl = grappe, X=1:length(W1),  FUN= function(X)  {which(W1[X] == cN12v)}, simplify = FALSE, USE.NAMES = TRUE ) 
+      parallel::stopCluster(grappe)
+    }
+    else {
+    I12 <- lapply(X=1:length(W1), FUN= function(X)  {which(W1[X] == cN12v)})
+    }
+    #  Compute sum of masses of each subset
+    V12_vec <- c(t(V12))
+    MAC <- unlist(lapply(X=1:length(I12), FUN= function(X)  {sum(V12_vec[I12[[X]]]) } ) )
+   # 
+   # Order W1 to put empty first and put masses in the same order
+   #  
+  # Sort_order
    sort_order<-order(unlist(lapply(X=1:length(W1_list), FUN = function(X) {shape(W1_list[[X]])} )))
+   # W1 sorted
    W1s <- W1_list[sort_order] # put W1_list in order
-   zframe <- W1s[[length(W1s)]]
+   #
    # Reconstruct tt from ssnames
-   # matrices will ne to big  this way
-   zz1 <- lapply(X=1:length(W1s), FUN = function(X) {outer(W1s[[X]], zframe, "==")})
-   zz2 <- mapply(FUN= function(X,Y) {lapply(X = 1:length(W1s), FUN = function(X) {reduction((zz1[[X]])[,Y], f="|")}) }, Y=1:length(zframe)) 
-   # if ( sum(unlist(zz2[1,])) == 0 ) {
-   #   zz2[1,] <- rep(0, length(zframe))
-   # }
-   # tt <- matrix(unlist(t(zz2)), nrow = dim(zz2)[1], byrow = TRUE)
-   # tt <- methods::as(tt, "RsparseMatrix")
-   # colnames(tt) <- zframe
-   # rownames(tt) <- nameRows(tt)
-   # #
-   # Reconstruct tt from ssnames Second method
+   #
+   zframe <- W1s[[length(W1s)]]
+   zz1 <- lapply(X=1:length(W1s), FUN= function(X) {lapply(X=1:length(W1s[[X]]), FUN = function(Y) {which(zframe == (W1s[[X]])[Y])}) } )
    #
    rowIdx <- vector()
    colIdx <- vector()
    for (i in 1:length(W1s) )  {
-     tempc <-  (unlist(zz2[i,])*(1:length(zframe)) )
+     tempc <-  (unlist(zz1[[i]]) )
      colIdx <- c(colIdx, tempc )
      tempr <- rep(i,sum(tempc >0) )
      rowIdx <- c(rowIdx, tempr)
    }
-   colIdx <- colIdx[colIdx>0]
-   # création de la matrice sparse
+   # Obtain sparse tt matrix of the result
    tt <- Matrix::sparseMatrix(
      i = rowIdx,
      j = colIdx, 
@@ -329,8 +322,8 @@ dsrwon<-function(x, y, mcores = "no", varnames = NULL, relnb = NULL, infovarname
    colnames(tt) <- zframe
    rownames(tt) <- nameRows(tt)
    #
-   # sparse tt
   ## 2.5.2 Identify if the empty set is present and define m_empty accordingly with it mass
+   #
   # Put masses in the same order as the ssnames list
   #
   z <- unlist(W1s[[1]])
@@ -389,13 +382,14 @@ dsrwon<-function(x, y, mcores = "no", varnames = NULL, relnb = NULL, infovarname
   #
   # construction of the result
   #
-  if ((!is.null(zx$tt)) & (!is.null(zy$tt) ) ) {
+  if (sparseM == FALSE) {
+  #if ((!is.null(zx$tt)) & (!is.null(zy$tt) ) ) {
  #  if (is.null(zx$ssnames) | is.null(zy$ssnames) ) {
-    z <- list(con = con, tt=tt, spec = spec, infovar = infovar, varnames = varnames, valuenames = valuenames, inforel = inforel, I12=I12, sort_order=sort_order, ssnames = NULL, sfod = NULL)
+    z <- list(con = con, tt=tt, spec = spec, infovar = infovar, varnames = varnames, valuenames = valuenames, inforel = inforel, I12=I12, sort_order=1:nrow(tt), ssnames = NULL, sfod = NULL)
   class(z) <- append(class(z), "bcaspec") 
   } 
   else {
-    z <- list(con = con, tt = tt, spec = spec, infovar = infovar, varnames = varnames, valuenames = valuenames, inforel = inforel, I12=I12, sort_order=sort_order, ssnames = W1_list[sort_order], sfod = zx$sfod)
+    z <- list(con = con, tt = tt, spec = spec, infovar = infovar, varnames = varnames, valuenames = valuenames, inforel = inforel, I12=I12, sort_order=1:nrow(tt), ssnames = W1_list[sort_order], sfod = zx$sfod)
     class(z) <- append(class(z), "bcaspec") 
   }
   return(z)
