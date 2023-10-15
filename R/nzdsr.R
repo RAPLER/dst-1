82c3974333f0f9590200d331ba1bfceed6dae613
Claude Boivin <webapp.cb@gmail.com>
2023-10-14 21:07:01 -0400

    Call to function ttmatrix to reconstruc description matrix from subsets names

--- R/nzdsr.R
+++ R/nzdsr.R
@@ -15,12 +15,12 @@
 #' print("combination of x1 and x2")
 #' x1x2 <- dsrwon(x1,x2, varname = "x")
 #' nzdsr(x1x2) 
-#' Test with sparse matrices
+#' # Test with sparse matrices
 #' x1s=x1
 #' x2s=x2
 #' x1s$tt <- methods::as(x1$tt, "RsparseMatrix")
 #' x2s$tt <- methods::as(x2$tt, "RsparseMatrix")
-#' x1x2s <- dsrwon(x1s, x2s, sparseM = TRUE)
+#' x1x2s <- dsrwon(x1s, x2s, use_ssnames = TRUE)
 #' nzdsr(x1x2s)
 #' 
 #' print("normalization of a bca definition.")
@@ -43,59 +43,25 @@ nzdsr<-function(x) {
     stop("Input argument not of class bcaspec.")
   }
   #
-  # 2023-06-12.
-  #
-  # Remove this test The conflict indice does not play a role in the combination by Dempster's rule. This is only a decision aid in the analysis of conflicting evidence
-  # if (x$con == 1) { 
-  #   stop('Completely conflicting evidence (con = 1). Data is inconsistent.')}
-  #
-  # End 2023-06-12
-  #
-  ## 2. Reconstruct I12 matrix (need to be updated if missing or if function addTobca has been used to add subsets)
-  #
-  # 2023-06-22 
-  #
-  # Put in comment all code pertaining to reconstruction of I12 and sort_order, using dsrwon. I12 and sort_order are is not necessary to this function when the bca inputted has not changed (by addTobca),
-  #
-  ## Start comment for I12
-  #
-  # if (is.null(x$ssnames) ) {
-  # nc <- ncol(x$tt)
-  # vacuous <- bca(matrix(rep(1, nc), nrow=1), m=1, cnames = colnames(x$tt))
-  # } else { 
-  #   vacuous <- bca(m = 1,  
-  #     varnames = "x", 
-  #     ssnames = list(x$ssnames[[length(x$ssnames)]]), sfod = x$sfod)
-  #   }
-  # vacuous$valuenames <- x$valuenames
-  # vacuous$infovar <- x$infovar
-  # x <- dsrwon(x,vacuous)
-  # 
-  # End comment for I12 
-  # case of tt matrix
-  #
-  # test 20230927 faire seulement avec tt matrice
-  #
- # if (is.null(x$ssnames)  ) {
+  # case of tt matrix missing
+  if (is.null(x$tt) ) {
+    x$tt <- ttmatrix(x$ssnames)
+  }
   #
   # 3. Assign variables
-  #
   w12<-cbind(x$spec[,2], x$tt)
   w1<- x$tt
   mac<-x$spec[,2]
   nc=ncol(w1) 
   #
   ## 2023-06-26 test reconstruct sort_order if missing
-  #
   if (is.null(x$sort_order)) {
   x$sort_order<-order(apply(x$tt,1,sum))
   }
-  # End 2023-06-28 Test
+  # End 2023-06-26 Test
   #
   tri<-x$sort_order
-  #
   # 4. remove empty set and normalize masses
-  #
   ind <- w12[tri[1],]
   if ((ind[1] != 0) & (sum(ind[-1]) == 0)) {
     empty <- tri[1]  
@@ -112,19 +78,16 @@ nzdsr<-function(x) {
   }
   #
   # 5. Update bca parameters 
-  #
   # tt matrix
   tt <- W2
   colnames(tt) <- colnames(x$tt)
   rownames(tt) <- nameRows(tt)
   #
   # spec parameter
-  #
   spec <- cbind((1:nrow(tt)), MACC)
   colnames(spec) <- c("specnb", "mass")
   #
   # infovar, varnames, valuenames, inforel parameters
-  #
   infovar <- x$infovar
   varnames <- x$varnames
   valuenames <- x$valuenames
@@ -133,46 +96,8 @@ nzdsr<-function(x) {
   colnames(inforel) <- c("relnb", "depth") 
   #
   # construction of the result
-  #
   z <- list(con = m_empty, tt = tt, spec = spec, infovar = infovar, varnames = varnames, valuenames = valuenames, inforel = inforel)
   class(z) <- append(class(z), "bcaspec")
   #
-  # fin test 20230927 
-  # partie ssnames en comment pour test
-  # } 
-  # else {
-  # #  
-  # # 4b. remove empty set and normalize masses
-  # #  
-  # MAC <- x$spec[,2]
-  # if (x$spec[1,2] != 0) {
-  #   MACC <- MAC[-1]/(1-MAC[1])  
-  #   } else {
-  #   MACC <- MAC
-  #   }
-  # #
-  # # 5b. Update bca parameters 
-  # #
-  # # ssnames
-  # ssnames <- x$ssnames[-1]
-  # #
-  # # spec parameter
-  # #
-  # spec <- cbind((1:(-1+length(x$ssnames)) ), MACC)
-  # colnames(spec) <- c("specnb", "mass") 
-  # #
-  # # infovar, varnames, valuenames, inforel parameters
-  # #
-  # infovar <- x$infovar
-  # varnames <- x$varnames
-  # relnb <- (x$inforel)[1,1]
-  # inforel <- matrix(c(relnb, nrow(infovar)), ncol = 2)
-  # colnames(inforel) <- c("relnb", "depth") 
-  # #
-  # # construction of the result
-  # #
-  # z <- list(con = m_empty, tt = NULL, spec = spec, infovar = infovar, varnames = varnames, valuenames = NULL, inforel = inforel)
-  # class(z) <- append(class(z), "bcaspec")
-  # }
   return(z)
     }
