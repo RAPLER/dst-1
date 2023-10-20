b4ebcdbbd6cb408816e10042cd06d27a5ca13149
Peiyuan Zhu <garyzhubc@gmail.com>
2023-10-20 09:41:58 -0700

    add simple support option to construction of tt, m from marginal

--- R/mFromMarginal.R
+++ R/mFromMarginal.R
@@ -2,13 +2,16 @@
 #' 
 #' @param marg_probs vector of marginal probabilities
 #' @param a=1e-5 probability that the sample is reliable
+#' @param min_prob=0 lower bound on marginal probabilities
+#' @param max_prob=2 upper bound on marginal probabilities
+#' @param simple=TRUE whether to use simple support function
 #' @return vector of probability masses obtained from uniformly sampling the cut
 #' @author Peiyuan Zhu
 #' @export
 #' @examples
 #' p <- c(2,2,1.5,1.2,1,0,0)
 #' mFromMarginal(p, FALSE)
-mFromMarginal <- function(marg_probs, a=1e-5, min_prob=0, max_prob=2) {
-  x <- a * diff(sort(unique(marg_probs))) / (max_prob - min_prob)
+mFromMarginal <- function(marg_probs, a=1e-5, min_prob=0, max_prob=2, simple=TRUE) {
+  x <- a * ifelse(simple, 1, diff(sort(unique(marg_probs))) / (max_prob - min_prob))
   return(c(x, 1 - sum(x))) 
 }
--- R/ttmatrixFromMarginal.R
+++ R/ttmatrixFromMarginal.R
@@ -1,13 +1,16 @@
 #' Construct tt matrix of a bca from marginal probabilities
 #' 
 #' @param marg_probs marginal probabilities
-#' @param from_above whether to cut marginal probabilities from above
+#' @param from_above=TRUE whether to cut marginal probabilities from above
+#' @param min_prob=0 lower bound on marginal probabilities
+#' @param max_prob=2 upper bound on marginal probabilities
+#' @param simple=TRUE whether to use simple support function
 #' @return matrix of possible subsets obtained from the cuts
 #' @author Peiyuan Zhu
 #' @export
 #' @examples
 #' p <- c(2,2,1.5,1.2,1,0,0)
 #' ttmatrixFromMarginal(p, FALSE)
-ttmatrixFromMarginal <- function(marg_probs, from_above=TRUE) {
-  return(do.call("rbind", lapply(sort(unique(marg_probs)), function(c) as.integer(if(from_above) marg_probs <= c else marg_probs >= c)))) 
+ttmatrixFromMarginal <- function(marg_probs, min_prob=0, max_prob=2, from_above=TRUE, simple=TRUE) {
+  return(do.call("rbind", lapply(if(simple) {c((max_prob-min_prob)/2, if(from_above) {max_prob} else {min_prob})} else {sort(unique(marg_probs))}, function(c) as.integer(if(from_above) marg_probs <= c else marg_probs >= c)))) 
 }
