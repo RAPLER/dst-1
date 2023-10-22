61c010f85396eeadbe7c2715de80a93c3efccc29
Peiyuan Zhu <garyzhubc@gmail.com>
2023-10-21 20:34:13 -0700

    move argument positions of ttmatrixFromMarginal, mFromMarginal

--- R/mFromMarginal.R
+++ R/mFromMarginal.R
@@ -3,16 +3,16 @@
 #' @param marg_probs vector of marginal probabilities
 #' @param from_above=TRUE whether to cut marginal probabilities from above
 #' @param a=1e-5 probability that the sample is reliable
+#' @param simple=TRUE whether to use simple support function
 #' @param min_prob=0 lower bound on marginal probabilities
 #' @param max_prob=2 upper bound on marginal probabilities
-#' @param simple=TRUE whether to use simple support function
 #' @return vector of probability masses obtained from uniformly sampling the cut
 #' @author Peiyuan Zhu
 #' @export
 #' @examples
 #' x <- c(2,2,1.5,1.2,1,0,0)
 #' mFromMarginal(x, FALSE)
-mFromMarginal <- function(marg_probs, a=1e-5, min_prob=0, max_prob=2, simple=FALSE) {
+mFromMarginal <- function(marg_probs, a=1e-5, simple=FALSE, min_prob=0, max_prob=2) {
   x <- a * (if(simple) 1 else diff(sort(unique(marg_probs))) / (max_prob - min_prob))
   return(c(x, 1 - sum(x))) 
 }
