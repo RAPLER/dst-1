5fa3aac0546aedad70bd42904435f4787d215623
Peiyuan Zhu <garyzhubc@gmail.com>
2023-10-24 17:54:21 -0700

    modify tests, rename functions having changed them

--- tests/testthat/test_ttmatrixPartition.R
+++ tests/testthat/test_ttmatrixPartition.R
@@ -3,6 +3,6 @@ library(dst)
 test_that("partition", {
   # test dimensions
   x <- bca(tt = matrix(c(1,1,0,1,1,1), nrow = 2, byrow = TRUE), m = c(0.8, 0.2), cnames = c(1,2,3))
-  pa <- partition(3, x$infovar[2])
+  pa <- ttmatrixPartition(3, x$infovar[2])
   expect_equal(dim(pa), c(3,3))
 })
