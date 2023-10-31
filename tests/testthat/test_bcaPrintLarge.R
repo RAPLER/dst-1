2b4065dd8c5f771a117d80e31ae711e52ca75eb2
Peiyuan Zhu <garyzhubc@gmail.com>
2023-10-30 18:02:49 -0700

    edit crime scene

--- tests/testthat/test_bcaPrintLarge.R
+++ tests/testthat/test_bcaPrintLarge.R
@@ -4,5 +4,5 @@ library(tidyverse)
 test_that("bcaPrintLarge", {
   # empty test
   x <- bca(tt = matrix(c(1,1,0,1,1,1), nrow = 2, byrow = TRUE), m = c(0.8, 0.2), cnames = c(1,2,3))
-  bcaPrintLarge(x)
+  expect_type(bcaPrintLarge(x), "character")
 })
