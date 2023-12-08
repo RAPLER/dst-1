75d0f0e2e4bc8702ed12193fe71c1f188be3f440
Peiyuan Zhu <garyzhubc@gmail.com>
2023-12-08 09:37:39 -0800

    add sparse option to ttmatrix

--- R/ttmatrix.R
+++ R/ttmatrix.R
@@ -1,6 +1,8 @@
 #' Construct a description matrix from a list of subsets names.
 #' 
 #' @param x A list of names
+#' @param sparse=c("yes","no") whether to use sparse matrix
+#' @param valuenames=NULL valuenames of bca
 #' @return ttmat A corresponding logical description matrix 
 #' @author Claude Boivin
 #' @examples 
@@ -8,7 +10,7 @@
 #' ttmatrix(subsets_names)
 #' @export
 #' 
-ttmatrix <- function(x) {
+ttmatrix <- function(x, sparse="no", valuenames=NULL) {
   # Obtain tt matrix from subsets names 
   #
   # Checks
@@ -17,9 +19,31 @@ ttmatrix <- function(x) {
     stop("Input must be a list.")
   }
   #
-z1l <- lapply(X = 1:length(x), FUN = function(X) {outer(x[[X]], x[[length(x)]], "==") } ) 
-ttmat <- t(mapply(FUN= function(X,Y) {unlist(lapply(X=1:ncol(z1l[[length(z1l)]]), FUN =  function(X) { reduction(z1l[[Y]][,X], f = "|")}) ) }, Y=1:length(x) ) )
-colnames(ttmat) <- c(x[[length(x)]])
-rownames(ttmat) <- nameRows(ttmat)
-return(ttmat)
+  if(sparse=="yes") {
+    if(is.null(valuenames)) {
+      stop("Need valuenames when sprase = \"yes\" ")
+    }
+    rowIdx <- vector()
+    colIdx <- vector()
+    for(i in 1:length(x)) {
+      rowIdx<-c(rowIdx,rep(i,length(x[[i]])))
+      colIdx<-c(colIdx,match(x[[i]],valuenames))
+    }
+    ttmat <- Matrix::sparseMatrix(
+      i = rowIdx,
+      j = colIdx, 
+      x = 1, 
+      dims = c(length(x), length(valuenames) )
+    )
+    ttmat <- methods::as(ttmat, "RsparseMatrix")
+    return(ttmat)
+  } else if(sparse=="no") {
+    z1l <- lapply(X = 1:length(x), FUN = function(X) {outer(x[[X]], x[[length(x)]], "==") } ) 
+    ttmat <- t(mapply(FUN= function(X,Y) {unlist(lapply(X=1:ncol(z1l[[length(z1l)]]), FUN =  function(X) { reduction(z1l[[Y]][,X], f = "|")}) ) }, Y=1:length(x) ) )
+    colnames(ttmat) <- c(x[[length(x)]])
+    rownames(ttmat) <- nameRows(ttmat)
+    return(ttmat)
+  } else {
+    stop("sparse can either be \"yes\" or \"no\" ")
+  }
 }
--- tests/testthat/test_ttmatrix.R
+++ tests/testthat/test_ttmatrix.R
@@ -1,7 +1,14 @@
 context("Construct a description matrix from a list of subsets names.")
 library(dst)
 test_that("ttmatrix", {
-# T1. input not a list
+  # T1. input not a list
   x1 <- c("a","b")
   expect_error(ttmatrix(x1), "Input must be a list.")
-  })
+  # T2. test boolean matrix
+  subsets_names <- list(c("b", "c"), "b", c("a", "b", "c"))
+  tt1<-ttmatrix(subsets_names)
+  expect_equal(as.logical(tt1[1,]),c(FALSE, TRUE, TRUE))
+  # T3. test sparse matrix
+  tt2<-ttmatrix(subsets_names, "yes", c("a","b","c"))
+  expect_equal(as.logical(tt2[1,]),c(FALSE, TRUE, TRUE))
+})
