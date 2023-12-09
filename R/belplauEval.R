d3392d82f3088090e09e64b591a3c49ec5f54938
Peiyuan Zhu <garyzhubc@gmail.com>
2023-12-08 16:50:12 -0800

    fix belplauEval, remove strict

--- R/belplauEval.R
+++ R/belplauEval.R
@@ -5,7 +5,6 @@
 #' @param var="rplau" variable name of the belplau to be used as ordering
 #' @param err="type I" type of error to be evaluated
 #' @param is_belplau=TRUE whether bel_plau is a belplau object
-#' @param strict=TRUE whether true_order distinguish elements > 0
 #' @return Type I, II error by comparing two orderings
 #' @author Peiyuan Zhu
 #' @export
@@ -20,7 +19,7 @@
 #' xy <- nzdsr(dsrwon(x,y))
 #' z<-belplau(xy,h=ttmatrixPartition(xy$infovar[2],xy$infovar[2]))
 #' belplauEval(z,c(0,1,0))
-belplauEval<-function(bel_plau,true_order,var="rplau",err="type I",is_belplau=TRUE,strict=TRUE) {
+belplauEval<-function(bel_plau,true_order,var="rplau",err="type I",is_belplau=TRUE) {
   # 1 means rplau > but actually <=
   # 0 means both > or both <
   # -1 means rplau < but actually >=
@@ -29,12 +28,12 @@ belplauEval<-function(bel_plau,true_order,var="rplau",err="type I",is_belplau=TR
   order_true<-outer(true_order,true_order,">")
   validated<-order_observed-order_true
   if (err=="type I") {
-    type_i<-validated[true_order>0,if(strict) (true_order==0)]
+    type_i<-validated[true_order>0,true_order==0]
     type_i_v<-as.vector(type_i)
     type_i_err<-1-sum(type_i_v==0)/length(type_i_v)
     return(type_i_err)
   } else if (err=="type II") {
-    type_ii<-validated[true_order==0,if(strict) (true_order>0)]
+    type_ii<-validated[true_order==0,true_order>0]
     type_ii_v<-as.vector(type_ii)
     type_ii_err<-1-sum(type_ii_v==0)/length(type_ii_v)
     return(type_ii_err)
