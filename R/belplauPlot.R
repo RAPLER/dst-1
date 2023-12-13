4ed054598f4082ac30413b52af1638971aea7c46
Peiyuan Zhu <garyzhubc@gmail.com>
2023-12-13 15:20:52 -0800

    remove legend title in belplauPlot

--- R/belplauPlot.R
+++ R/belplauPlot.R
@@ -4,8 +4,6 @@
 #' @param xlab: x-axis labels e.g. c("1:34","35:68","69:101")
 #' @param color: color of xlab e.g. c(0,1,0)
 #' @param y="rplau": column name of belplau matrix
-#' @param legend_title="": title of legend
-#' @param n_breaks=20: number of break points on the x-axis
 #' @param is_log_scale=TRUE: whether to use log-scale
 #' @return a plot of belplau matrix
 #' @author Peiyuan Zhu
@@ -16,8 +14,8 @@
 #' cnames = c("a", "b", "c"), varnames = "x", idvar = 1)
 #' bel_plau <- belplau(bpa)
 #' belplauPlot(bel_plau)
-belplauPlot<-function(belplau_mat, xlab, color, y="rplau", x="index", legend_title="", n_breaks=20, is_log_scale=TRUE) {
+belplauPlot<-function(belplau_mat, xlab, color, y="rplau", x="index", is_log_scale=TRUE) {
   ggplot(as.data.frame(belplau_mat) %>% mutate(!!sym(y):=(if(is_log_scale) log(!!sym(y)) else !!sym(y))) %>% mutate(!!x:=factor(xlab, xlab))) +
     geom_point(aes(x=!!sym(x),y=!!sym(y),colour=factor(color, unique(color)))) + 
-    labs(color=legend_title) + scale_x_discrete(breaks = n_breaks)+ theme_bw() + ylab(if (is_log_scale) paste0("log(",y,")") else y) 
+    labs(color="") + theme_bw() + ylab(if (is_log_scale) paste0("log(",y,")") else y) 
 }
