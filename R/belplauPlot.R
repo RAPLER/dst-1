959608b572676512c60306f4764aad930fc2e85c
Peiyuan Zhu <garyzhubc@gmail.com>
2023-12-14 13:19:47 -0800

    fix problem of binding character and numerical matrix in belplauPlot

--- R/belplauPlot.R
+++ R/belplauPlot.R
@@ -18,8 +18,52 @@
 #' cnames = c("a", "b", "c"), varnames = "x", idvar = 1)
 #' bel_plau <- belplau(bpa)
 #' belplauPlot(bel_plau)
-belplauPlot<-function(belplau_mat, xlab, color, y="rplau", x="index", levels=NULL, legend_title="", main_title="",is_log_scale=TRUE,is_negative=FALSE) {
-  ggplot(as.data.frame(cbind(belplau_mat,color)) %>% mutate(!!sym(y):=(if(is_log_scale) log(!!sym(y)) * (if(is_negative) -1 else 1) else !!sym(y))) %>% mutate(!!sym(x):=xlab) %>% mutate(color=factor(color, if (is.null(levels)) unique(color) else levels) )%>% arrange(desc(color))) +
-    geom_point(aes(x=!!sym(x),y=!!sym(y),colour=color)) + 
-    labs(title=main_title,color=legend_title) + theme_bw() + ylab(if (is_log_scale) paste0(if(is_negative) "-","log(",y,")") else y) 
+belplauPlot <- function(belplau_mat,
+                        xlab,
+                        color,
+                        y = "rplau", x = "index",
+                        levels = NULL,
+                        legend_title = "",
+                        main_title = "",
+                        is_log_scale = TRUE,
+                        is_negative = FALSE) {
+  if (is.null(levels)) levels <- unique(color)
+  
+  dat <- as.data.frame(belplau_mat) |>
+    mutate(
+      !!sym(x) := xlab,
+      color = factor(color, levels = levels)
+    )
+  
+  if (is_log_scale) {
+    ylab <- paste0("log(", y, ")")
+    .mult <- 1
+    if (is_negative) {
+      .mult <- -1
+      ylab <- paste0("-", ylab)
+    }
+    mutate(
+      dat, !!sym(y) := log(!!sym(y)) * .mult
+    )
+  } else {
+    ylab <- y
+  }
+  
+  dat$order <- reorder(dat$color, rep(1, nrow(dat)), FUN = length)
+  
+  dat <- dat[order(dat$order, decreasing = TRUE), ]
+  
+  ggplot(dat) +
+    geom_point(
+      aes(
+        x = !!sym(x),
+        y = !!sym(y),
+        colour = color
+      )
+    ) +
+    labs(
+      title = main_title, color = legend_title,
+      ylab = ylab
+    ) +
+    theme_bw()
 }
--- tests/testthat/test_belplauPlot.R
+++ tests/testthat/test_belplauPlot.R
@@ -8,5 +8,5 @@ test_that("belplauPlot", {
   bel_plau_plot <- belplauPlot(bel_plau, c("1","2","3"), c(0,1,0), levels=c(0,1),is_log_scale=TRUE,is_negative = TRUE)
   bel_plau_plot <- belplauPlot(matrix(rep(1,101),ncol=1,dimnames=list(NULL,"rplau")), 1:101, c(rep(0,50),1,rep(0,50)), levels=c(1,0),is_log_scale=FALSE,is_negative = FALSE)
   bel_plau_plot <- belplauPlot(matrix(rep(1,101),ncol=1,dimnames=list(NULL,"rplau")), 1:101, c(rep("B",50),"A",rep("B",50)), levels=c("A","B"),is_log_scale=FALSE,is_negative = FALSE)
-  expect_equal(bel_plau_plot$data$index[1], "1")
+  expect_equal(bel_plau_plot$data$index[1], 1)
 })
