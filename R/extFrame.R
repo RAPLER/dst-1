2a8b6355659abb1ac60447f4f75911f4669de0db
Claude Boivin <webapp.cb@gmail.com>
2024-01-31 21:36:00 -0500

    Add doc for parameter x

--- R/extFrame.R
+++ R/extFrame.R
@@ -2,14 +2,15 @@
 #' 
 #' This function works on a basic chance assignment (bca) \code{x} defined on a single variable. Iy=t allows the addition of new values to the frame of discernment. 
 #' 
-#' @param z An object of bca class, i.e. a basic chance assignment defined on one variable
+#' @param x An object of bca class, i.e. a basic chance assignment defined on one variable
 #' @param use_ssnames Default= FALSE. Put TRUE if use of subset names is wanted.
 #' @param lab A character vector containing the names of the elements to add to the frame of discernment.
 #' @return zxtnd The bca with its frame extended
 #' @author Claude Boivin
 #' @export
 #' @examples 
-#' s1_e1 <- bca(tt = matrix(c(1,0,1,1),nrow = 2, byrow = TRUE), m = c(0.6,0.4), cnames = c("S1","S2"), varnames = "v1", idvar = 1) 
+#' s1_e1 <- bca(tt = matrix(c(1,0,1,1),nrow = 2, byrow = TRUE), 
+#' m = c(0.6,0.4), cnames = c("S1","S2"), varnames = "v1", idvar = 1) 
 #' s13_names <- extFrame(s1_1, lab = "S3", use_ssnames =TRUE)
 #' s13 <- extFrame(s1_1, lab = "S3")
 extFrame <- function(x, use_ssnames = FALSE, lab = NULL) {
