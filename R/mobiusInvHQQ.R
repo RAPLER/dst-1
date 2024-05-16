57556be3d5edf15c1e845df11953b0d6a18fa399
Peiyuan Zhu <30366447+garyzhubc@users.noreply.github.com>
2024-05-16 10:00:15 -0700

    Update mobiusInvHQQ.R
    Use while loop instead of for loop for smaller memory

--- R/mobiusInvHQQ.R
+++ R/mobiusInvHQQ.R
@@ -12,12 +12,14 @@
 #' mobiusInvHQQ(qq, matrix(c(0,1,0,1,1,0), nrow = 2, byrow = TRUE))
 mobiusInvHQQ<-function(qq,h){
   m <- 0
+  i <- 0
   # go through all supersets as h union a combination of elements from the complement
-  for (i in 0:(2**(length(h) - sum(h)) - 1)) {
+  while (i < (2**(length(h) - sum(h)) - 1)) {
     hh <- h
     dhh <- encode(rep(2, length(h) - sum(h)), i)
     hh[which(h == 0)] <- dhh
     m <- m + (-1) ** sum(dhh) * qq(hh)
+    i <- i + 1
   }
   return(m)
 }
