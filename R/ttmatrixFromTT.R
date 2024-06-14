cd1e2ed6a9f4ca70d2b011376fb8a8ffc2239386
Peiyuan Zhu <30366447+garyzhubc@users.noreply.github.com>
2024-06-14 11:51:04 -0700

    Update ttmatrixFromTT

---
+++ R/ttmatrixFromTT.R
@@ -0,0 +1,16 @@
+#' Construct a tt matrix of focal elements from a list of tt matrices
+#' 
+#' @param TT List of tt matrices
+#' @return tt A corresponding logical description matrix of the focal elements
+#' @author Peiyuan Zhu
+#' @export
+#' @examples 
+#' TT<-list(matrix(c(0,1,1,1,1,0,1,1,1),nrow = 3, byrow = TRUE),
+#'  tt = matrix(c(1,0,0,1,1,1),nrow = 2, byrow = TRUE))
+#' ttmatrixFromTT(TT)
+ttmatrixFromTT <- function(TT) {
+  tt<-Reduce(function(x,y) { res<-inters(x,y); res<-res[!duplicated(res),]; return(res)},TT)
+  sort_order <-order(apply(tt,1,sum))
+  tt<-tt[sort_order,]
+  return(tt)
+}
