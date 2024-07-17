f58088389d7b9cddca84ec2dc5645f30d9b82353
Peiyuan Zhu <30366447+garyzhubc@users.noreply.github.com>
2024-07-17 13:02:56 -0700

    Modify description of belplauEval, belplauPlot

--- R/belplauEval.R
+++ R/belplauEval.R
@@ -7,11 +7,11 @@
 #'  \item Total error: the sum of error of the first kind and the error of the second kind
 #' }
 #' 
-#' @param bel_plau belplau matrix or a numerical vector quantifying order of importance of the elements of the frame.
+#' @param belplau_mat belplau matrix e.g. belplau(bca) or a numerical vector quantifying order of importance of the elements of the frame.
 #' @param true_order a binary vector representing the truth. 1 means relevant and 0 means not relevant.
-#' @param var ="rplau" column name of the belplau matrix to be used as ordering.
-#' @param err ="first kind" kind of error to be evaluated. Can also take value "second kind" or "total".
-#' @param is_belplau=TRUE whether bel_plau is indeed a belplau matrix or just a numerical vector quantifying order of importance of elements.
+#' @param var = "rplau" column name of the belplau matrix to be used as ordering.
+#' @param err = "first kind" kind of error to be evaluated. Can also take value "second kind" or "total".
+#' @param is_belplau = TRUE whether bel_plau is indeed a belplau matrix or just a numerical vector quantifying order of importance of elements.
 #' @return A number of first, second kind, or total error.
 #' @author Peiyuan Zhu
 #' @export
@@ -26,8 +26,8 @@
 #' xy <- nzdsr(dsrwon(x,y))
 #' z<-belplau(xy,h=ttmatrixPartition(xy$infovar[2],xy$infovar[2]))
 #' belplauEval(z,c(0,1,0))
-belplauEval<-function(bel_plau,true_order,var="rplau",err="first kind",is_belplau=TRUE) {
-  if (is_belplau) observed_order<-bel_plau[,var] else observed_order<-bel_plau
+belplauEval<-function(belplau_mat,true_order,var="rplau",err="first kind",is_belplau=TRUE) {
+  if (is_belplau) observed_order<-belplau_mat[,var] else observed_order<-belplau_mat
   order_observed <- outer(observed_order,observed_order,">")
   order_true <- outer(true_order,true_order,">")
   validated <- order_observed-order_true
--- R/belplauPlot.R
+++ R/belplauPlot.R
@@ -1,17 +1,17 @@
 #' Plot belplau matrix
 #' 
-#' @param belplau_mat Belplau matrix e.g. belplau(bpa)
+#' @param belplau_mat Belplau matrix e.g. belplau(bpa) or a numerical vector quantifying order of importance of the elements of the frame
 #' @param xlab X-axis labels e.g. c("1:34","35:68","69:101")
 #' @param color Color of xlab e.g. c(0,1,0)
-#' @param y ="rplau": column name of belplau matrix
-#' @param x ="index":TO BE DOCUMENTED
-#' @param levels =NULL: levels of color in order
-#' @param main_title ="": main title
-#' @param legend_title ="": title of legend
-#' @param is_log_scale =TRUE Whether to use log-scale
-#' @param is_negative =TRUE Whether to multiple by -1
-#' @param is_factor =FALSE Whether to plot all x labels
-#' @return a plot of belplau matrix
+#' @param y = "rplau": column name of belplau matrix
+#' @param x = "index": x-axis name
+#' @param levels = NULL: levels of color in order
+#' @param main_title = "": main title
+#' @param legend_title = "": title of legend
+#' @param is_log_scale = TRUE Whether to use log-scale
+#' @param is_negative = TRUE Whether to multiple by -1
+#' @param is_factor = FALSE Whether to plot all x labels
+#' @return a plot of a column of the belplau matrix or a numerical vector quantifying order of importance of the elements of the frame
 #' @author Peiyuan Zhu
 #' @importFrom rlang := sym
 #' @importFrom stats reorder quantile
