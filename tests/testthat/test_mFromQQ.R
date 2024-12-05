f95bf8ed365972715168e6348a529782cb981f64
Peiyuan Zhu <30366447+garyzhubc@users.noreply.github.com>
2024-12-04 16:53:49 -0800

    Update test_mFromQQ.R

--- tests/testthat/test_mFromQQ.R
+++ tests/testthat/test_mFromQQ.R
@@ -30,11 +30,11 @@ test_that("mFromQQ", {
   tty <- matrix(c(0,1,1,1,1,1), nrow = 2, 
                 byrow = TRUE)
   
-  x <- bca(tt = ttx, m = c(0.4, 0.6), W2c = tty, method="ezt",
+  x <- bca(tt = ttx, m = c(0.4, 0.6), method="ezt",
            cnames = c("a", "b", "c"), varnames = "x", idvar = 1)
-  y <- bca(tt =  tty, m = c(0.3, 0.7), W2c = ttx, method="ezt",  
+  y <- bca(tt =  tty, m = c(0.3, 0.7), method="ezt",  
            cnames = c("a", "b", "c"),  varnames = "y", idvar = 2)
-  z <- dsrwon(x,y,use_qq = TRUE)
+  z <- dsrwon(x,y,use_qq = TRUE,method="ezt")
   
   # Test 1.2.2 mFromQQ vs fmt
   m1 <- mFromQQ(w$qq,3,cnames=c("a", "b", "c"))
